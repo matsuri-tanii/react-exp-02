81d27bb0a977c83c1971311f92d46d88f1201db1
matsuri-tanii <matsuri.tanii@gmail.com>
2026-01-19 23:27:58 +0900

    add calendaritem chart header sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,21 @@
-import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Chart from "./pages/chart";
 
-import './App.css'
-import News from './components/News';
+
+
 
 function App() {
-  const [name, setName] = useState("名前入力");
-  const [email, setEmail] = useState("メールアドレス入力");
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  useEffect(()=>{
-    console.log("起動しました！！！");
-  },[]);
-
-
-  console.log("順番の確認");
-
+  
   return (
     <>
       {/*  */}
-      <News />
-
-      <div>
-        <p>名前が入ります</p>
-        <input 
-          type="text" 
-          placeholder="名前を入力してください" 
-          value={name} 
-          onChange={handleNameChange}
-        />
-      </div>
-      <div>
-        <p>メールアドレスが入ります</p>
-        <input 
-          type="text" 
-          placeholder="メールアドレスを入力してください" 
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chart" element={<Chart />} />
+        </Routes>
+      </Router>
 
       {/*  */}
     </>
---
+++ src/_App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+import News from './components/News';
+
+
+
+function App() {
+  const [name, setName] = useState("名前入力");
+  const [email, setEmail] = useState("メールアドレス入力");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  useEffect(()=>{
+    console.log("起動しました！！！");
+  },[]);
+
+
+  console.log("順番の確認");
+
+  return (
+    <>
+      {/*  */}
+
+      <News />
+
+      <div>
+        <p>名前が入ります</p>
+        <input 
+          type="text" 
+          placeholder="名前を入力してください" 
+          value={name} 
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        <p>メールアドレスが入ります</p>
+        <input 
+          type="text" 
+          placeholder="メールアドレスを入力してください" 
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+
+      {/*  */}
+    </>
+  )
+}
+
+export default App
